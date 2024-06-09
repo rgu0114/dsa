667f8c755ea6dd320a3cc4e3e09981be791cda66
rgu0114 <richardgu0114@gmail.com>
2024-06-08 20:44:47 -0700

    finish sorting

--- sorting/intArrays.cpp
+++ sorting/intArrays.cpp
@@ -83,3 +83,98 @@ static void insertionSort(vector<int> &b, int h, int k) {
     i++;
   }
 }
+
+static int partition(vector<int> &b, int h, int k) {
+  int j = h;
+  int i = k;
+  // 
+        //          h---------------------------k
+        // pre:  b |x|     ?                     |  for some x
+        //          -----------------------------
+        // 
+        //          h---------------------------k
+        // post: b |   <= x      |x|     >= x    |
+        //          -----------------------------
+        // {inv P: b[h+1..i-1] <= b[h] = x <= b[j+1..k]}, i.e.
+        //  
+        //          h---------j-------i------------k
+        // post: b |   <= x  |x|  ?    |    >= x    |
+        //          --------------------------------
+  while (j < i) {
+    if (b[j + 1] <= b[j]) {
+      // next element smaller than or equal to 'x', should be to the left of j index
+      int temp = b[j + 1];
+      b[j + 1] = b[j];
+      b[j] = temp;
+      j++;
+    } else {
+      // next element greater than 'x', should be in position i & update i
+      int temp = b[i];
+      b[i] = b[j + 1];
+      b[j + 1] = temp;
+      i--;
+    }
+  }
+  return j;
+}
+
+/** Sort b[h..k] */
+static void quickSort(vector <int> &b, int h, int k) {
+  if (k + 1 - h < 2) return;
+  int mid = partition(b, h, k);
+  quickSort(b, h, mid - 1);
+  quickSort(b, mid + 1, k);
+}
+
+/** Segments b[h..e] and b[e+1..k] are already sorted.<br>
+    Permute their values so that b[h..k] is sorted.
+           h--------------e-------------k
+    pre: b|    sorted     |    sorted    |
+           -------------------------------
+            h----------------------------k
+    inv:  b|    sorted  |                 |
+           -------------------------------
+            h--------------e-------------k
+    post: b|            sorted            |
+            ------------------------------
+*/
+static void merge(vector <int> &b, int h, int e, int k) {
+  int a[e + 1 - h] = {};
+
+  for (int i = 0; i < e + 1; i++) {
+    a[i] = b[i];
+  }
+
+  int a_pt = 0;
+  int b_pt = e + 1;
+  int pt = h;
+
+  while (a_pt < e + 1 && b_pt < k) {
+    if (a[a_pt] < b[b_pt]) {
+      b[pt] = a[a_pt];
+      a_pt++;
+    } else {
+      b[pt] = b[b_pt];
+      b_pt++;
+    }
+    pt++;
+  }
+
+  if (a_pt < e + 1) {
+    while (a_pt < e + 1) {
+      b[pt] = a[a_pt];
+      a_pt;
+      pt++;
+    }
+  }
+}
+
+
+/** Sort b[h..k]. */
+static void mergeSort(vector<int> &b, int h, int k) {
+  if (h >= k) return;
+  int med = (h + k) / 2;
+  mergeSort(b, h, med);
+  mergeSort(b, med + 1, k);
+  merge(b, h, med, k);
+}
